2f675369a8082f18603c2d5ac47687d70ec80b81
buiok <consciousness.is.disabled@gmail.com>
2022-06-30 19:29:04 +0500

    create db
    create model: category, material, tag

---
+++ database/migrations/2022_06_30_130524_create_materials_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('materials', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('author');
+            $table->string('type');
+            $table->text('description');
+
+            $table->foreignId('category_id')->constrained()->ondelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('materials');
+    }
+};
