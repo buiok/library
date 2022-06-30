1be2bd33bff61136b6ff3e3569c7aca9e75ea4c6
buiok <consciousness.is.disabled@gmail.com>
2022-07-01 02:17:08 +0500

    add basic pages
    add MaterialController

---
+++ app/Http/Controllers/MaterialController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Material;
+use Illuminate\Http\Request;
+use App\Models\Tag;
+
+class MaterialController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $materials = Material::all();
+        return view('material.index', compact('materials'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('material.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Material  $material
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Material $material)
+    {
+        $tags = Tag::all();
+        return view('material.show', compact('material'), compact('tags'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Material  $material
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Material $material)
+    {
+        return view('material.edit');
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Material  $material
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Material $material)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Material  $material
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Material $material)
+    {
+        //
+    }
+}
---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/bootstrap-utilities.css">
+    <link rel="stylesheet" href="../css/style.css">
+
+    <title> @yield('title') </title>
+</head>
+<body>
+<div class="main-wrapper">
+    <div class="content">
+        @include('inc.menu')
+        @yield('content')
+    </div>
+    <footer class="footer py-4 mt-5 bg-light">
+        <div class="container">
+            <div class="row">
+                <div class="col text-muted">Test</div>
+            </div>
+        </div>
+    </footer>
+</div>
+
+@yield('hidden')
+
+<!-- Optional JavaScript; choose one of the two! -->
+
+<!-- Option 1: Bootstrap Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4"
+        crossorigin="anonymous"></script>
+
+<!-- Option 2: Separate Popper and Bootstrap JS -->
+
+</body>
+</html>
