7c8307173dd5caa742a797d452a70a724a5478f0
buiok <consciousness.is.disabled@gmail.com>
2022-07-14 18:57:33 +0500

    review in TagController.php

--- app/Http/Controllers/TagController.php
+++ app/Http/Controllers/TagController.php
@@ -3,16 +3,20 @@
 namespace App\Http\Controllers;
 
 use App\Models\Tag;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 class TagController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return View
      */
-    public function index()
+    public function index(): View
     {
         $tags = Tag::orderBy('name')->get();
         return view('tag.index', compact('tags'));
@@ -21,9 +25,9 @@ class TagController extends Controller
     /**
      * Show the form for creating a new resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return View
      */
-    public function create()
+    public function create(): View
     {
         return view('tag.create');
     }
@@ -31,12 +35,12 @@ class TagController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @return RedirectResponse
      */
-    public function store(Request $request)
+    public function store(Request $request): RedirectResponse
     {
-        $validatedData = $request->validate(['name' => 'required|unique:tags,name']);
+        $request->validate(['name' => 'required|unique:tags,name']);
         Tag::create($request->all());
 
         return redirect()->route('tags.index')->with('success', 'Тег добавлен');
@@ -45,10 +49,10 @@ class TagController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\Material  $material
-     * @return \Illuminate\Http\Response
+     * @param Tag $tag
+     * @return Application|Factory|\Illuminate\Contracts\View\View
      */
-    public function show(Tag $tag)
+    public function show(Tag $tag): \Illuminate\Contracts\View\View|Factory|Application
     {
         $materials = $tag->materials;
         return view('tag.show', compact('materials', 'tag'));
@@ -57,10 +61,10 @@ class TagController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\Models\Tag  $tag
-     * @return \Illuminate\Http\Response
+     * @param Tag $tag
+     * @return Application|Factory|\Illuminate\Contracts\View\View
      */
-    public function edit(Tag $tag)
+    public function edit(Tag $tag): \Illuminate\Contracts\View\View|Factory|Application
     {
         return view('tag.edit', compact('tag'));
     }
@@ -68,13 +72,13 @@ class TagController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Tag  $tag
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @param Tag $tag
+     * @return RedirectResponse
      */
-    public function update(Request $request, Tag $tag)
+    public function update(Request $request, Tag $tag): RedirectResponse
     {
-        $validatedData = $request->validate(['name' => 'required|unique:tags,name']);
+        $request->validate(['name' => 'required|unique:tags,name']);
         $tag->update($request->all());
 
         return redirect()->route('tags.index')->with('success', 'Тег изменен');
@@ -83,10 +87,10 @@ class TagController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Models\Tag  $tag
-     * @return \Illuminate\Http\Response
+     * @param Tag $tag
+     * @return RedirectResponse
      */
-    public function destroy(Tag $tag)
+    public function destroy(Tag $tag): RedirectResponse
     {
         $tag->delete();
         return redirect()->route('tags.index')->with('success', 'Тег удален');
