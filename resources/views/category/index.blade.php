d6739f074d64e6aa831e8b6134f6730998387937
buiok <106178856+buiok@users.noreply.github.com>
2022-09-02 11:05:06 +0500

    add absence condition of object

--- resources/views/category/index.blade.php
+++ resources/views/category/index.blade.php
@@ -8,7 +8,10 @@
     <div class="container">
         <h1 class="my-md-5 my-4">Категории</h1>
         <a class="btn btn-primary mb-4" href="{{ route('categories.create') }}" role="button">Добавить</a>
-        <div class="row">
+        @if (count($categories) == 0 )
+            <div><b>Категории отсутствуют</b></div>
+        @else
+            <div class="row">
             <div class="col-md-6">
                 <ul class="list-group mb-4">
                     <li class="list-group-item d-flex justify-content-between">
@@ -22,12 +25,14 @@
                             {{ $category->name }}
                         </a>
                         <span class="text-nowrap">
+
                         <a href="{{ route('categories.edit', $category->id) }}" class="text-decoration-none me-2">
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
                                  class="bi bi-pencil" viewBox="0 0 16 16">
                             <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/>
                             </svg>
                         </a>
+
                         <form style='display:inline;' action="{{ route('categories.destroy', $category->id) }}" method="POST">
                             @csrf
                             @method('DELETE')
@@ -45,6 +50,7 @@
                     @endforeach
                 </ul>
             </div>
+        @endif
         </div>
     </div>
 @endsection
