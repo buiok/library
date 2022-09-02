9713bc8e4ea2ae14965c440e9d1263721a043106
buiok <106178856+buiok@users.noreply.github.com>
2022-09-02 11:01:29 +0500

    delete Illuminate\Http\Request

--- app/Http/Controllers/TagController.php
+++ app/Http/Controllers/TagController.php
@@ -7,7 +7,6 @@ use App\Models\Tag;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Http\RedirectResponse;
-use Illuminate\Http\Request;
 use Illuminate\View\View;
 
 class TagController extends Controller
